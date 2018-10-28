74a2365796062b049f4d18e87b860d9aa5c8b65e
huitre <gohin.j@gmail.com>
2018-10-28 17:48:21 +0100

    exo 1

--- exo2/src/index.1.js
+++ exo2/src/index.1.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './style.css';
 
+class Resultat extends React.Component {
+  render () {
+    const { children, ...rest } = this.props;
+    return (
+      <div className="resultat" {...rest}>
+        {children}
+      </div>
+    )
+  }
+}
+
 class Calculette extends React.Component {
   render() {
     return (
       <div className="calculette">
-        Ma calculette
+        <Resultat>0</Resultat>
       </div>
     );
   }
